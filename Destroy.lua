ecaa3528ff5ec430bed10eeaaa61e0922578b4ec
美波 <anatawahitoka@gmail.com>
2025-07-13 08:22:43 +0900

    Update Destroy.lua

--- Destroy.lua
+++ Destroy.lua
@@ -1,1 +1,10 @@
-game:GetService("CoreGui"):WaitForChild("RobloxGui"):WaitForChild("Core"):Destroy()
+task.wait(1)
+local coreGui = game:GetService("CoreGui")
+local success, robloxGui = pcall(function()
+    return coreGui:WaitForChild("RobloxGui")
+end)
+if success and robloxGui:FindFirstChild("Core") then
+    local successDestroy = pcall(function()
+        robloxGui.Core:Destroy()
+    end)
+end
